aa25f0884e72f0a972796a76f6cdb8bd13e5da56
ikuto <ikuto0608@me.com>
2017-09-23 14:40:45 -0700

    remove not unused migrations

--- db/migrate/20170923211220_rename_user_id_to_user_location.rb
+++
@@ -1,5 +0,0 @@
-class RenameUserIdToUserLocation < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :user_locations, :users_id, :user_id
-  end
-end
--- db/migrate/20170923211230_rename_location_id_to_user_location.rb
+++
@@ -1,5 +0,0 @@
-class RenameLocationIdToUserLocation < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :user_locations, :locations_id, :location_id
-  end
-end
