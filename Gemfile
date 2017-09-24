e76263ccacae401ce3f2bb979bbd6fe53a44cd51
ikuto <ikuto0608@me.com>
2017-09-23 19:59:05 -0700

    add cors

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'annotate'
 
 gem 'jwt'
 
+gem 'rack-cors', :require => 'rack/cors'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,12 @@ module Nile
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths << Rails.root.join('lib')
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
